d6a693f1fd2d2132318273b317b4bfc98624f87f
Pasquale Morabito <61754975+Pasq04@users.noreply.github.com>
2023-01-11 22:26:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let loginBtn = document.getElementById('login');
 loginBtn.addEventListener("click", () => {
     window.location.href = process.env.NODE_ENV !== 'production' ? 
                             'http://localhost:5500/login' :
-                            'https://own--music.herokuapp.com/login';
+                            'https://pasq04.github.io/OwnMusic/login';
 })

c2a85d8be9e51802211646867dc9495991d1b045
Pasquale Morabito <61754975+Pasq04@users.noreply.github.com>
2023-01-11 22:27:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let loginBtn = document.getElementById('login');
 
 loginBtn.addEventListener("click", () => {
-    window.location.href = process.env.NODE_ENV !== 'production' ? 
+    /*window.location.href = process.env.NODE_ENV !== 'production' ? 
                             'http://localhost:5500/login' :
-                            'https://pasq04.github.io/OwnMusic/login';
+                            'https://pasq04.github.io/OwnMusic/login'; */
+    window.location.href = 'https://pasq04.github.io/OwnMusic/login';
 })
